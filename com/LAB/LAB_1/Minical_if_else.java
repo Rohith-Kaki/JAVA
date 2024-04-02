f354a8d7ef2f31bc82be6b1a9a8566cfb459c106
Rohith Kaki <rohithkaki11@gmail.com>
2024-04-02 09:10:19 +0530

    Mini calculator (if-else)

---
+++ com/LAB/LAB_1/Minical_if_else.java
@@ -0,0 +1,39 @@
+package LAB.LAB_1;
+import java.util.Scanner;
+
+public class Minical_if_else {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter two numbers:");
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter the operator (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Error: Division by zero!");
+                return;
+            }
+        } else {
+            System.out.println("Invalid operator!");
+            return;
+        }
+
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
