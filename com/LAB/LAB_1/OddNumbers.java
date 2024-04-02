810df5d41424683900dc6584ef0acfc25fcb45d9
Rohith Kaki <rohithkaki11@gmail.com>
2024-04-02 09:11:53 +0530

    Odd Numbers

---
+++ com/LAB/LAB_1/OddNumbers.java
@@ -0,0 +1,12 @@
+package LAB.LAB_1;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+        int N = 100;
+        for(int i=1; i<=N; i++){
+            if(i%2 != 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
