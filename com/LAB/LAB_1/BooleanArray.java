5aa2e85f297886688ab5bf3878fc7d587d0f937c
Rohith Kaki <rohithkaki11@gmail.com>
2024-04-03 09:25:14 +0530

    Boolean Array

---
+++ com/LAB/LAB_1/BooleanArray.java
@@ -0,0 +1,13 @@
+package LAB.LAB_1;
+
+import java.util.Arrays;
+
+public class BooleanArray {
+    public static void main(String[] args) {
+        boolean [][] arr = {
+            {true,false,true},
+            {false,true,false}
+        };
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
