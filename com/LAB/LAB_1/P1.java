8a864115d8d84e3ee19f0b2ec115b878f049997c
Rohith Kaki <rohithkaki11@gmail.com>
2024-04-11 11:12:35 +0530

    Basics

---
+++ com/LAB/LAB_1/P1.java
@@ -0,0 +1,10 @@
+package LAB.LAB_1;
+
+public class P1 {
+    public static void main(String[] args) {
+System.out.println((101+0)|3);
+System.out.println(3.0e-6 * 10000000.1);
+System.out.println((false && false) || (true && true));
+System.out.println((false && false) && (true && true));
+    }
+}
