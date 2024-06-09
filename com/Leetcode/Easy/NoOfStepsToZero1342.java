8bd9e1c3664b143584f47733bc4db8d2585ab817
Rohith Kaki <rohithkaki11@gmail.com>
2024-06-09 07:41:48 +0530

    No of Steps to Zero 1342

---
+++ com/Leetcode/Easy/NoOfStepsToZero1342.java
@@ -0,0 +1,19 @@
+package Leetcode.Easy;
+
+public class NoOfStepsToZero1342{
+    public static void main(String[] args) {
+        System.out.println(NoOfSteps(57));
+    }
+    public static int NoOfSteps(int num){
+        return StepCounter(num, 0);
+    }
+    public static int StepCounter(int num, int steps){
+        if(num == 0){
+            return steps;
+        }
+        if(num % 2 == 0){
+            return StepCounter(num / 2, steps + 1);
+        }
+        return StepCounter(num - 1, steps + 1);
+    }
+}
