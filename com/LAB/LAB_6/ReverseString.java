519846c98353ec569cb2db9bf338c8ad81aaac1f
Rohith Kaki <rohithkaki11@gmail.com>
2024-06-17 10:17:20 +0530

    Reverse a String

---
+++ com/LAB/LAB_6/ReverseString.java
@@ -0,0 +1,11 @@
+package LAB.LAB_6;
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        System.out.println("Reversed string: " + reversed);
+    }
+}
