a86c45cad1835f5bad1bff8a80b12c06738772e9
Rohith Kaki <rohithkaki11@gmail.com>
2024-07-05 12:51:14 +0530

    About Final Keyword

---
+++ com/Programs/Aboutfinal.java
@@ -0,0 +1,46 @@
+// Java Program to demonstrate Different
+// Ways of Initializing a final Variable
+
+// Main class 
+class Aboutfinal {
+  
+    // a final variable
+    // direct initialize
+    final int THRESHOLD = 5;
+    
+    // a blank final variable
+    final int CAPACITY;
+    
+    // another blank final variable
+    final int  MINIMUM;
+    
+    // a final static variable PI
+    // direct initialize
+    static final double PI = 3.141592653589793;
+    
+    // a  blank final static  variable
+    static final double EULERCONSTANT;
+    
+    // instance initializer block for 
+    // initializing CAPACITY
+    {
+        CAPACITY = 25;
+    }
+    
+    // static initializer block for 
+//The blank final variable can be static also which will be initialized in the static block only.
+
+    static{
+        EULERCONSTANT = 2.3;
+    }
+    
+    // constructor for initializing MINIMUM
+    // Note that if there are more than one
+    // constructor, you must initialize MINIMUM
+    // in them also
+    // public Aboutfinal() 
+    // {
+    //     MINIMUM = -1;
+    // }
+        
+}
