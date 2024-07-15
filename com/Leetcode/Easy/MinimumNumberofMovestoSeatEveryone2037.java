86ba741e283bad01c0c94936eca1c5c257c8b011
Rohith Kaki <rohithkaki11@gmail.com>
2024-07-15 11:03:23 +0530

    Min no of moves to seat everyone 2037

---
+++ com/Leetcode/Easy/MinimumNumberofMovestoSeatEveryone2037.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans  = 0;
+        for(int i=0;i<seats.length;i++){
+            ans += Math.abs(seats[i] - students[i]);
+        }
+        return ans;
+    }
+}
+
+public class MinimumNumberofMovestoSeatEveryone2037 {
+    public static void main(String[] args) {
+        int [] seats = {3,4,5,2,1};
+        int [] students = {4,5,2,5,3};
+        Solution one = new Solution();
+        System.out.println(one.minMovesToSeat(seats,students));
+    }
+}
