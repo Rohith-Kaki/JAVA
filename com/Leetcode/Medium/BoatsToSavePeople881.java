410381a7cba496323c2778f4a1a4d5cef2bbf343
Rohith Kaki <rohithkaki11@gmail.com>
2024-07-18 10:26:02 +0530

    Added implementation

--- com/Leetcode/Medium/BoatsToSavePeople881.java
+++ com/Leetcode/Medium/BoatsToSavePeople881.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 
 public class BoatsToSavePeople881 {
     public static void main(String[] args) {
-        
+        Solution one = new Solution();
+        int[] people = {11,3,4,3,4,5,4,3};
+        System.out.println(one.numRescueBoats(people, 5));
+
     }
 }
 class Solution {
