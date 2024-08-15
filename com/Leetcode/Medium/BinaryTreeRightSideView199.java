2f4a9e421e6b6dd72d2636349cb9b4b1d497286c
Rohith Kaki <rohithkaki11@gmail.com>
2024-08-15 11:18:32 +0530

    Added Q199

---
+++ com/Leetcode/Medium/BinaryTreeRightSideView199.java
@@ -0,0 +1,51 @@
+package Leetcode.Medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+public class BinaryTreeRightSideView199 {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(! q.isEmpty()){
+            int qsize = q.size();
+            for(int i=0;i<qsize;i++){
+                TreeNode current = q.poll();
+                if(i == qsize - 1){
+                    result.add(current.val);
+                }
+                if(current.left != null){
+                    q.offer(current.left);
+                }
+                if(current.right != null){
+                    q.offer(current.right);
+                }
+            
+            }
+        }
+        return result;
+    }
+} 
+}
