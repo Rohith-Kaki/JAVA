c1e5b32a14984c48c1c6f6ee378038f5333b64d8
Rohith Kaki <rohithkaki11@gmail.com>
2024-09-03 00:00:10 +0530

    Added

--- com/Leetcode/Easy/MinimumNumberofMovestoSeatEveryone2037.java
+++ com/Leetcode/Easy/MinimumNumberofMovestoSeatEveryone2037.java
@@ -16,7 +16,7 @@ public class MinimumNumberofMovestoSeatEveryone2037 {
     public static void main(String[] args) {
         int [] seats = {3,4,5,2,1};
         int [] students = {4,5,2,5,3};
-        Solution one = new Solution();
+        CycleDetection one = new CycleDetection();
         System.out.println(one.minMovesToSeat(seats,students));
     }
 }
